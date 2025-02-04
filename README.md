a6e4e6afc4d390e0adc100da4ffc6ddd8df013a5
shushu <shushu@LAPTOP-VB81MAAH>
2025-02-04 22:09:04 +0800

    Assignment 6: BVH加速结构(SAH未完成)

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Assignment 6 加速结构
+## 编译
+```bash
+mkdir build
+cd build
+cmake ..
+make
+```
+
+## 运行
+```bash
+./Raytracing
+```
+
+## 运行结果
+![](images/binary.png)
+
+## 学习笔记
+- 在 `Triangle::getIntersection` 中，用 $t$ 来计算交点比用 重心坐标 计算**更快**
+- 在 `Bounds3::IntersectP` 中，如果光线的x分量为负方向，则计算出来的 `txmin` 和 `txmax` 的大小会颠倒，所以要先交换后才能计算出正确结果
+- `BVHAccel::getIntersection`的代码根据课程的伪代码写即可
+![](images/BVHAccel.png)
--- Triangle.hpp
+++ Triangle.hpp
@@ -215,26 +215,32 @@ inline Intersection Triangle::getIntersection(Ray ray)
     if (dotProduct(ray.direction, normal) > 0)
         return inter;
     double u, v, t_tmp = 0;
-    Vector3f pvec = crossProduct(ray.direction, e2);
+    Vector3f pvec = crossProduct(ray.direction, e2); // S1
     double det = dotProduct(e1, pvec);
     if (fabs(det) < EPSILON)
         return inter;
 
     double det_inv = 1. / det;
-    Vector3f tvec = ray.origin - v0;
+    Vector3f tvec = ray.origin - v0; // S
     u = dotProduct(tvec, pvec) * det_inv;
     if (u < 0 || u > 1)
         return inter;
-    Vector3f qvec = crossProduct(tvec, e1);
+    Vector3f qvec = crossProduct(tvec, e1); // S2
     v = dotProduct(ray.direction, qvec) * det_inv;
     if (v < 0 || u + v > 1)
         return inter;
     t_tmp = dotProduct(e2, qvec) * det_inv;
 
     // TODO find ray triangle intersection
-
-
-
+    if (t_tmp > 0) {
+        inter.happened = true;
+        // inter.coords = (1 - u - v) * v0 + u * v1 + v * v2;
+        inter.coords = ray(t_tmp);
+        inter.normal = normal;
+        inter.distance = t_tmp;
+        inter.obj = this;
+        inter.m = m;
+    }
 
     return inter;
 }
